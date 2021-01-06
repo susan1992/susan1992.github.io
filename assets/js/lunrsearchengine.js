f93bf2a17bf8cc26cd34040febe28362d0c400da
Wu, Yuchen <yuchen.wu@sap.com>
2021-01-06 21:59:00 +0100

    new template

---
+++ assets/js/lunrsearchengine.js
@@ -0,0 +1,109 @@
+
+var documents = [{
+    "id": 0,
+    "url": "/404.html",
+    "title": "404",
+    "body": "404 Page does not exist!Please use the search bar at the top or visit our homepage! "
+    }, {
+    "id": 1,
+    "url": "/about",
+    "title": "Mediumish Template for Jekyll",
+    "body": "This website is built with Jekyll and Mediumish template for Jekyll. It's for demonstration purposes, no real content can be found. Mediumish template for Jekyll is compatible with Github pages, in fact even this demo is created with Github Pages and hosted with Github.  Documentation: Please, read the docs here. Questions or bug reports?: Head over to our Github repository! Buy me a coffeeThank you for your support! Your donation helps me to maintain and improve Mediumish . Buy me a coffee Documentation"
+    }, {
+    "id": 2,
+    "url": "/categories",
+    "title": "Categories",
+    "body": ""
+    }, {
+    "id": 3,
+    "url": "/",
+    "title": "Home",
+    "body": "{% if page. url == “/” %}       Featured:       {% for post in site. posts %}    {% if post. featured == true %}      {% include featuredbox. html %}    {% endif %}  {% endfor %}  {% endif %}       All Stories:         {% for post in paginator. posts %}    {% include postbox. html %}    {% endfor %}    {% include pagination. html %}"
+    }, {
+    "id": 4,
+    "url": "/robots.txt",
+    "title": "",
+    "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
+    }, {
+    "id": 5,
+    "url": "/episode2/",
+    "title": "从四川小村庄到德国汉堡，普通姑娘Lena越努力越幸运的不普通人生",
+    "body": "2020/09/30 - 在线收听: 海外听众点这里: 国内听众点这里: 笔记和链接: 本期嘉宾是在德国做互惠生，又继续在这里读研究生的Lena。我们聊了她在互惠生家庭和德国男朋友的两段生活中遭遇的文化冲突，也聊了Lena从中国农村到德国大城市这一路的感悟。: 🕙时间轴 00:30 Lena在互惠生家庭遭遇的culture shock 03:40 简短的自我介绍 04:05 什么是互惠生？ 06:18 如果你也是这种情况，考虑下互惠生项目吧 07:38 和德国家庭一起住，Lena学到了如何与德国人相处 11:00 两件有些心酸的小事，德国的“规则”没那么绝对 19:00 为什么选择来德国？ 20:50 来自家庭的支持 22:31 申请学校 23:20 FH大学的实践性更强吗？ 24:36 和德国男朋友在一起也免不了文化冲突 28:53 回首往事，越努力越幸运Credit感谢刚考完试正在搬家的Lena抽出时间和我一起录了两个多小时的采访。9月一直在参加培训，疯狂加班，这期节目剪了一个多月。感谢Lena的容忍，还没和我绝交😂 🔗Links 豆瓣互惠生小组 https://www. make-it-in-germany. com/en/ 来德国，找一份工作：聊聊德国留学和找工作签证 https://mp. weixin. qq. com/s?__biz=MzI0ODczNTE3Ng==&amp;mid=2247483805&amp;idx=1&amp;sn=db27fd9288e7a64acc55d7931238d99e 德国FH和Uni两种大学的区别，1000字讲清楚德国教育体系 https://mp. weixin. qq. com/s?__biz=MzI0ODczNTE3Ng==&amp;mid=2247484009&amp;idx=1&amp;sn=5c8c2be8db2608fa3f07abb18f903ca2 德国互惠生官方信息     德国联邦劳动局关于Aupair的介绍（德语） https://www. arbeitsagentur. de/unternehmen/arbeitskraefte/au-pair   Au-pair Society e. V. . 德国互惠生协会 https://www. aupair-society. de   经德国互惠生协会审查后成为会员的德国互惠生中介 https://www. aupair-society. de/unseremitglieder   德国联邦劳动局关于Aupair的介绍（英语） https://www. arbeitsagentur. de/datei/au-pair-in-germany-en_ba012998. pdf   德国驻中国使领馆，互惠生签证申请须知 https://china. diplo. de/blob/1341632/d12749addf98e05d81c44ca4157264fc/pdf-merkblatt-natvisum-aupair-data. pdf   "
+    }, {
+    "id": 6,
+    "url": "/episode1/",
+    "title": "从中关村软件园到德国，程序员也可以拥有诗和远方",
+    "body": "2020/08/10 - 在线收听: 海外听众点这里: 国内听众点这里: 笔记和链接: 本期嘉宾是从中关村搬家到德国的程序员老吴，我们聊了他来德国后工作和吃喝玩乐上的改变，最后介绍了他是如何通过找工作签证在德国找到了工作。: 🕙时间轴 00:22 老吴介绍他在国内的工作经历 01:05 在德国上班和在国内上班哪里不一样？ 03:27 选择离开的理由 04:41 出国之后，人人都是中华小当家 05:35 一家之言：什么样的人更适合来德国？ 06:39 谁说好山好水好寂寞，听听我们都在德国玩点什么？ 10:21 来德国的动机 13:25 来德国的准备：辞职，学德语 16:43 找工作签证 Job Seeking Visa （JSV） 18:32 在德国找工作，英语还是德语？ 19:45 语言有多重要？第二个面试 23:26 拿到offer！ 30:06 如果你要来德国，给你一个建议Credit感谢素未谋面的老吴和我一起花了两个多小时录制这期节目，为这期节目取了有诗意的名字🙏感谢小人物播客的阿乐，阿乐录制了一系列播客新手入门教学视频，对一个从没用过音频剪辑软件的人来说非常有帮助。 🔗Links 德国工作移民官方网站 https://www. make-it-in-germany. com/en/ 来德国，找一份工作：聊聊德国留学和找工作签证 https://mp. weixin. qq. com/s?__biz=MzI0ODczNTE3Ng==&amp;mid=2247483805&amp;idx=1&amp;sn=db27fd9288e7a64acc55d7931238d99e 找工作签证 JSV     德国工作移民官方网站关于找工作签证的介绍 https://www. make-it-in-germany. com/en/visa/kinds-of-visa/jobseekers/   德国驻中国使领馆，找工作签证申请须知 https://china. diplo. de/blob/1341664/ba2b3dec2afaff81d872572ead3244fd/pdf-merkblatt-natvisum-arbeitsplatzsuche-data. pdf   "
+    }, {
+    "id": 7,
+    "url": "/salary/",
+    "title": "在德国做码农，能挣多少钱",
+    "body": "2020/05/31 - 先声明：本文内薪酬信息有和在相关公司就职的小伙伴求证过，但所有信息均来自公开数据，没有泄漏公司机密个人隐私。 先划一条baseline: 求职网站Stepstone的2019年薪酬报告指出： IT行业平均薪酬为€60545/年 高等学历教育毕业的雇员的平均薪酬为€65172/年 其中，软件工程师的薪酬随着工作经验的增长，平均工资如下：       工作经验   平均税前工资（欧元/年）         &lt;2年   €46393       3-5年   €54931       6-10年   €60582       &gt;10年   €67975   LinkedIn也调查过德国软件工程师的薪资：       工作经验   中位数税前工资（欧元/年）         &lt;1年   €48000       1-5年   €51000       6-14年   €60000       &gt;15年   €64400    到底能赚多少钱？: 先介绍下四家公司代表： SAP：德国最大的软件公司，主营业务为企业资源管理软件（ERP），在全世界均有业务。 Zalando：德国最大的服装电商，业务遍布欧洲，近年来一直有从海外招聘的案例。 博世（Bosch）：全球最大的汽车零部件供应商之一，主营业务包括汽车（硬件和软件），消费品（包括家用电器和电动工具），工业技术（包括驱动和控制）以及能源和建筑技术。 亚马逊（Amazon）：一家疫情中股价飞涨的公司。 这四家公司对德国IT从业人员来说分属于四种类型： SAP - 传统软件公司Zalando - 互联网公司博世 - 非IT行业公司，制造业公司亚马逊 - 美企 一个软件工程师在这四家公司中可以期待的薪酬（含奖金）       公司/工作经验   &lt;1   1-3   4-6   7-9         SAP   €51611   €56715   €63621   €71538       Zalando   €51812   €54706   €61871   €67318       博世   €58819   €64641   €73382           亚马逊   €60128   €62150   €72150   €72254    随便说说的小结：: 两家传统的德国公司SAP和Bosch严格遵循了工龄越长工资越高，每年涨幅3%的德国薪酬默认规律。 亚马逊遵循着在美国的规则，薪水和工作经验没有直接关系。Zalando这样的互联网公司也模仿美企的套路。 美国背景的互联网公司工资似乎都高一点。 德国汽车行业相关的公司工资高、福利好，只是说德语的比例高。 平均来看，大公司的工资比小公司高。 薪酬也和公司的地理位置相关：法兰克福、慕尼黑、斯图加特、汉堡&gt;杜塞尔多夫、柏林。当然工资高的城市房价也高，工资不高的城市也可能房价高（柏林）。 挺多德国公司都在某个大城市附近的小村小镇。想住大房子养狗养娃的可以考虑下这类公司：SAP、博世。 要努力争取每一次谈薪水和奖金的机会。 大幅度加薪的三条道路：升职、内部转岗、跳槽。 跳槽需谨慎：每年换一家公司的人不太受欢迎。 "
+    }];
+
+var idx = lunr(function () {
+    this.ref('id')
+    this.field('title')
+    this.field('body')
+
+    documents.forEach(function (doc) {
+        this.add(doc)
+    }, this)
+});
+function lunr_search(term) {
+    document.getElementById('lunrsearchresults').innerHTML = '<ul></ul>';
+    if(term) {
+        document.getElementById('lunrsearchresults').innerHTML = "<p>Search results for '" + term + "'</p>" + document.getElementById('lunrsearchresults').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><br /><span class='body'>"+ body +"</span><br /><span class='url'>"+ url +"</span></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>No results found...</li>";
+        }
+    }
+    return false;
+}
+
+function lunr_search(term) {
+    $('#lunrsearchresults').show( 400 );
+    $( "body" ).addClass( "modal-open" );
+    
+    document.getElementById('lunrsearchresults').innerHTML = '<div id="resultsmodal" class="modal fade show d-block"  tabindex="-1" role="dialog" aria-labelledby="resultsmodal"> <div class="modal-dialog shadow-lg" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" class="close" id="btnx" data-dismiss="modal" aria-label="Close"> &times; </button> </div> <div class="modal-body"> <ul class="mb-0"> </ul>    </div> <div class="modal-footer"><button id="btnx" type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button></div></div> </div></div>';
+    if(term) {
+        document.getElementById('modtit').innerHTML = "<h5 class='modal-title'>Search results for '" + term + "'</h5>" + document.getElementById('modtit').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><br /><small><span class='body'>"+ body +"</span><br /><span class='url'>"+ url +"</span></small></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>Sorry, no results found. Close & try a different search!</li>";
+        }
+    }
+    return false;
+}
+    
+$(function() {
+    $("#lunrsearchresults").on('click', '#btnx', function () {
+        $('#lunrsearchresults').hide( 5 );
+        $( "body" ).removeClass( "modal-open" );
+    });
+});
